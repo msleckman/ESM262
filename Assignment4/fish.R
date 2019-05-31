9168191f947d4954f7f8832587ad3e40b422993f
msleckman <msleckman@ucsb.edu>
2019-05-30 23:41:58 -0700

    final

--- Assignment4/fish.R
+++ Assignment4/fish.R
@@ -1,64 +1,42 @@
 #' Function for ESM 262 assignment 4
 #' @title Fish function 
-#' @description 
-#' @param 
-#' @param 
-#' @param 
-#' @return most frequently caught fish ineach location, total revenue for each lcoation, total fisheries revenue sum
-#' 
-
-
-fish_type <- c("cod", "salmon", "tuna",
-               "trout", "halibut", 
-               "macherel", "sardines")
-
-fish_price <- c(10, 15, 12, 8, 17, 7, 3)
-
-site_A_count = c(100, 130, 200, 56, 89, 240, 310)
-site_B_count = c(45, 211, 330, 43, 91, 252, 349)
-site_C_count = c(80, 120, 253, 21, 87, 235, 413)
-site_D_count = c(143, 90, 146, 60, 101, 210, 291)
-
-
-fish_cost_df <- data.frame(fish_type, fish_price)
-
-nfish_df <- data.frame(fish_type, site_A_count,
-                       site_B_count, site_C_count,
-                       site_D_count)
-nfish_df
-
-
-library(reshape2)
-library(tidyverse)
-
-
-site_D_frequ <- nfish_df %>%
-  select(fish_type, site_D_count) %>% 
-  filter(site_D_count == max(site_D_count)) %>% 
-  melt(id.vars = "fish_type")
-
-site_D_frequ
-
-fish_frequ <- data.frame(Site, fish_type, count)   
-
+#' @description calculated the most frequent fish caught, the total revenue from fish catch per location (with or without graph),    
+#' @param fish_count_df a dataframe with count of different fish type at each location.
+#' @param fish_cost_df a dataframe with cost of unit fish by type 
+#' @param graph if True, the function outputs a graphical representation of the total fisheries revenue for each site based on type and number of fish caught. Default is false.
+#' @return Function returns 1.most frequently caught fish ineach location, 2.Total revenue for each lcoation, 3. total fisheries revenue sum for all locations 4. If graph = T, a the final graph as explained above
+#' @author Margaux Sleckman 
 
 fish_summary <- function(fish_count_df, fish_cost_df, graph = F){
+
+## Error checking 1: Verify that fish_type column for fish_count_df and fish_cost_df are same and sort
   
-if(fish_count_df[,1] != fish_cost_df[,1]){
+  for(i in nrow(fish_count_df)){
+    
+    if (fish_count_df[,1][i] == fish_cost_df[,1][i]){
+      
+      sort(as.character(fish_count_df[,1]), decreasing = F)
+      sort(as.character(fish_cost_df[,1]), decreasing = F)
+      
+    }
+  }  
+  
+  
+##Error Checking 2: if no fish price for specific fish type, show warning
+    
+for(i in nrow(fish_count_df)){
+  
+if(fish_count_df$fish_type[i] != fish_cost_df$fish_type[i]){
 
-  warning("fish types are not same")
+  stop("Missing price for 1 ore more of recorded fish types")
 
 }
-## Verify that fish_type column for fish_count_df and fish_cost_df are same
-
-if (fish_count_df[,1] == fish_cost_df[,1]){
-
-sort(as.character(fish_count_df[,1]), decreasing = F)
-sort(as.character(fish_cost_df[,1]), decreasing = F)
 
 }
+  
 
-### separate by location
+### 1. Max Frequency 
+## Separate by location
   site_A_frequ <- fish_count_df %>%
     select(fish_type, site_A_count) %>% 
     filter(site_A_count == max(site_A_count)) %>% 
@@ -75,40 +53,35 @@ sort(as.character(fish_cost_df[,1]), decreasing = F)
     select(fish_type, site_D_count) %>% 
     filter(site_D_count == max(site_D_count)) %>% 
     melt(id.vars = "fish_type")
-    
+ 
+## get df of highest caught fish     
 max_frequency <- rbind(site_A_frequ, site_B_frequ, site_C_frequ, site_D_frequ) 
 colnames(max_frequency) <- c("fish_type", "site_id", "frequency")
 
-##output
-max_frequency_output <- list("Most frequently caught fish per site", max_frequency[c("site_id", "fish_type", "frequency")])  
+### 2. Total revenue for each location
 
-
-### total revenue for each location
-
+## df revenue by fish
 revenue_by_fish = data.frame(fish = fish_count_df$fish_type,
                     A = fish_count_df[,2] * fish_cost_df[,2],
                     B = fish_count_df[,3] * fish_cost_df[,2], 
                     C = fish_count_df[,4] * fish_cost_df[,2],
                     D = fish_count_df[,5] * fish_cost_df[,2]
                     )
-
+## transpose
 revenue_by_fish = melt(revenue_by_fish, id = "fish")
 
 colnames(revenue_by_fish) <- c("fish", "Site", "Count")
 
-
+## get sum of revenue per location
 revenue <- revenue_by_fish %>% 
   group_by(Site) %>% 
   summarise(total_revenue = sum(Count))
 
-##output
-revenue_output <- list("Total fisheries revenue per site", as.data.frame(revenue))
-
-### Total fish revenue sum 
+### 3. Total fish revenue sum 
 
 total_fisheries_revenue = sum(revenue$total_revenue)
 
-### Graph
+### 4. Graph
 
 revenue_graph <- as.data.frame(revenue)
 
@@ -121,20 +94,23 @@ final_graph <- ggplot(revenue_graph, aes(x=Site, y=total_revenue))+
   xlab("Site")+
   labs(title = "Total Revenue by Fishing Location\n")
 
+### Returns 
 
+## return with graph
 if(graph == T){
 
-  return(list(max_frequency_output, revenue_output, paste0("Total Fisheries Revenue = ", total_fisheries_revenue), final_graph))
+  return(list(`Most frequently_caught fish per site` = max_frequency, `Total Fisheries Revenue per Site` = as.data.frame(revenue), paste0("Total Fisheries Revenue = ", total_fisheries_revenue), `Graph Revenue per site` = final_graph))
 
-   }
+}
+
+## return without graph
  else {
  
-   return(list(max_frequency, revenue_output, paste0("Total Fisheries Revenue = ", total_fisheries_revenue)))
+   return(list(`Most frequently_caught fish per site` = max_frequency, `Total Fisheries Revenue per Site` = as.data.frame(revenue), paste0("Total Fisheries Revenue = ", total_fisheries_revenue)))
+   
  
    }
 
 }
 
-fish_summary(fish_count_df = nfish_df, fish_cost_df = fish_cost_df, graph = T)
-fish_summary(fish_count_df = nfish_df, fish_cost_df = fish_cost_df, graph = F)
 
